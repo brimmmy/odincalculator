198ea3a5224a9e64879884a97e300cc0fd066fc5
brimmmy <alyse.brimhall@gmail.com>
2022-03-02 16:38:54 +0100

    Operate function added and working

--- javascript.js
+++ javascript.js
@@ -11,3 +11,16 @@ function divide(numberOne, numberTwo){
     return numberOne / numberTwo;
 };
 
+function operate(sign, one, two){
+    if (sign == '+') {
+         return add(one, two);
+    } else if (sign == '-') {
+         return subtract(one, two);
+    } else if (sign == '*') {
+        return multiply(one, two);
+    } else if (sign == '/') {
+        return divide(one, two);
+    } else {
+        return "ERRoR";
+    }
+};
