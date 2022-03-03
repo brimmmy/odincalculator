b38e08804ef10702f3d7f84aa331e0ff346b35ed
brimmmy <alyse.brimhall@gmail.com>
2022-03-03 18:14:31 +0100

    Basic, two number functions working. Must clear after each evaluation.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,118 @@
-function add(numberOne, numberTwo){
-    return numberOne + numberTwo;
+let screen = [];
+let signOne;
+let firstNumber;
+let secondNumber;
+
+function populateNine() {
+    screen.push("9");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
 };
-function subtract(numberOne, numberTwo){
-    return numberOne - numberTwo;
+function populateEight() {
+    screen.push("8");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
 };
-function multiply(numberOne, numberTwo){
-    return numberOne * numberTwo;
+function populateSeven() {
+    screen.push("7");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
 };
-function divide(numberOne, numberTwo){
-    return numberOne / numberTwo;
+function populateSix() {
+    screen.push("6");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populateFive() {
+    screen.push("5");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populateFour() {
+    screen.push("4");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populateThree() {
+    screen.push("3");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populateTwo() {
+    screen.push("2");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populateOne() {
+    screen.push("1");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populateZero() {
+    screen.push("0");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populateDot() {
+    screen.push(".");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+};
+function populatePlus() {
+    screen.push("+");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+    signOne = '+';
+};
+function populateMinus() {
+    screen.push("-");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+    signOne = '-';
+};
+function populateMultiply() {
+    screen.push("*");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+    signOne = '*';
+};
+function populateDivide() {
+    screen.push("/");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+    console.log(screen);
+    signOne = '/';
 };
 
+function firstArray() {
+    screen.pop();
+    console.log(screen);
+    firstNumber = Number(screen.join(""));
+    screen.push(signOne);
+    screen = [];
+};
+
+function secondArray() {
+    secondNumber = Number(screen.join(""));
+};
+
+function add(numberOne, numberTwo){
+    document.getElementById("displayScreen").innerHTML = numberOne + numberTwo;
+ };
+ function subtract(numberOne, numberTwo){
+    document.getElementById("displayScreen").innerHTML = numberOne - numberTwo;
+ };
+ function multiply(numberOne, numberTwo){
+    document.getElementById("displayScreen").innerHTML = numberOne * numberTwo;
+ };
+ function divide(numberOne, numberTwo){
+    document.getElementById("displayScreen").innerHTML = numberOne / numberTwo;
+ };
+
+
 function operate(sign, one, two){
+    sign = signOne;
+    one = firstNumber;
+    two = secondNumber;
     if (sign == '+') {
          return add(one, two);
     } else if (sign == '-') {
@@ -25,65 +126,6 @@ function operate(sign, one, two){
     }
 };
 
-let screen = [];
-function populateNine() {
-    screen.push("9");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateEight() {
-    screen.push("8");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateSeven() {
-    screen.push("7");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateSix() {
-    screen.push("6");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateFive() {
-    screen.push("5");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateFour() {
-    screen.push("4");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateThree() {
-    screen.push("3");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateTwo() {
-    screen.push("2");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateOne() {
-    screen.push("1");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateZero() {
-    screen.push("0");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateDot() {
-    screen.push(".");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-
-function populatePlus() {
-    screen.push("+");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateMinus() {
-    screen.push("-");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateMultiply() {
-    screen.push("*");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
-};
-function populateDivide() {
-    screen.push("/");
-    document.getElementById("displayScreen").innerHTML = screen.join("");
+function clearScreen(){
+    window.location.reload();
 };
