d624cb46d3bd4afe8cc159429a41a047fc28c610
brimmmy <alyse.brimhall@gmail.com>
2022-03-03 12:38:46 +0100

    Display screen populates with onclick functions

--- javascript.js
+++ javascript.js
@@ -24,3 +24,66 @@ function operate(sign, one, two){
         return "ERRoR";
     }
 };
+
+let screen = [];
+function populateNine() {
+    screen.push("9");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateEight() {
+    screen.push("8");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateSeven() {
+    screen.push("7");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateSix() {
+    screen.push("6");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateFive() {
+    screen.push("5");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateFour() {
+    screen.push("4");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateThree() {
+    screen.push("3");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateTwo() {
+    screen.push("2");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateOne() {
+    screen.push("1");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateZero() {
+    screen.push("0");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateDot() {
+    screen.push(".");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+
+function populatePlus() {
+    screen.push("+");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateMinus() {
+    screen.push("-");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateMultiply() {
+    screen.push("*");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
+function populateDivide() {
+    screen.push("/");
+    document.getElementById("displayScreen").innerHTML = screen.join("");
+};
